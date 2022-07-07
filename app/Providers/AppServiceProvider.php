1bfa596bb2814e7477a5877dae224534fc3fc474
heinminaye <heinminaye8000@gmail.com>
2022-07-07 20:58:23 +0630

    finish

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,19 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($module = $this->getModule(func_get_args())) {
-            $this->app["config"]->package("app/" . $module, app_path() . "/modules/" . $module . "/config");
-
-            // Add routes
-            $routes = app_path() . "/modules/" . $module . "/routes.php";
-            if (file_exists($routes))
-                require $routes;
-        }
-    }
-    public function getModule($args)
-    {
-        $module = (isset($args[0]) and is_string($args[0])) ? $args[0] : null;
-        return $module;
+        //
     }
 
     /**

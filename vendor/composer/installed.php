0c8e64aad9e02fcf85f5b5fdf390994c134e243b
heinminaye <heinminaye8000@gmail.com>
2022-07-07 11:56:20 +0630

    add

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'db67163841e3cd7c3783732cc37c2bf1b767bf3c',
+        'reference' => '816d88a7c3cb5eff6ce8a38d8e8d655ca11810f3',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -406,7 +406,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'db67163841e3cd7c3783732cc37c2bf1b767bf3c',
+            'reference' => '816d88a7c3cb5eff6ce8a38d8e8d655ca11810f3',
             'dev_requirement' => false,
         ),
         'laravel/sail' => array(
@@ -790,8 +790,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
